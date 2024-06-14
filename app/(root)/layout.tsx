cc7bae094ca8dcea3cb0073018272566d785572b
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-14 12:28:31 -0400

    Updated sidebar to include icons and styling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
-
+import Sidebar from "@/components/ui/sideBar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+const loggedIn = {firstName: 'John', lastName: 'Doe'}
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );

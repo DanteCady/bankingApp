44a46e491b8f51f48e0be45d6b2574c7d3d4bcd8
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-14 16:02:02 -0400

    Added and updated modvilenav component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,28 @@
-import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
-import Sidebar from "@/components/ui/sideBar";
+import type { Metadata } from 'next';
+import { Inter, IBM_Plex_Serif } from 'next/font/google';
+import Sidebar from '@/components/ui/sideBar';
+import Image from 'next/image';
+import MobileNav from '@/components/ui/mobileNavbar';
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
+	const loggedIn = { firstName: 'John', lastName: 'Doe' };
 
-const loggedIn = {firstName: 'John', lastName: 'Doe'}
-
-  return (
-    <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn} />
-        {children}
-    </main>
-  );
+	return (
+		<main className="flex h-screen w-full font-inter">
+			<Sidebar user={loggedIn} />
+			<div className="flex size-full flex-col">
+				<div className="root-layout">
+					<Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+				<div>
+					<MobileNav user={loggedIn} />
+				</div>
+				</div>
+			{children}
+      </div>
+		</main>
+	);
 }

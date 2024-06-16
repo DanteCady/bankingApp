28a914b739c082c7fceaab136ba61638f5a8b46b
DanteCady <37083142+DanteCady@users.noreply.github.com>
2024-06-16 14:43:41 -0400

    Updated auth layout to include app image

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
-
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -8,8 +8,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="flex min-h-screem w-full justify-between font-inter">
         {children}
+        <div className="auth-asset">
+          <div>
+            <Image
+              src="/icons/auth-image.svg"
+              alt="auth image"
+              width={500}
+              height={500}
+            />
+          </div>  
+        </div>
     </main>
   );
 }
